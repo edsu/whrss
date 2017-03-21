bcc1d67ea8d96c6b162c0c8c58766c8fa59bb403
Ed Summers <ehs@pobox.com>
2017-03-21 01:36:48 +0000

    ignore 1600daily, it will need it's own rss feed

--- whrss.py
+++ whrss.py
@@ -9,6 +9,11 @@ def get_item(e):
     created = created.strftime("%a, %d %b %Y %H:%M:%S +0000")
     a = e.select('a')[0]
     url = 'https://www.whitehouse.gov' + a['href']
+
+    # ignore links to this page which changes every day and isn't a permalink
+    if url == "https://www.whitehouse.gov/1600daily":
+        return None
+
     title = a.text
     return """<item><title>%s</title><link>%s</link><guid>%s</guid><description>%s</description><pubDate>%s</pubDate></item>""" % (title, url, url, title, created)
 
@@ -18,6 +23,8 @@ doc = BeautifulSoup(html, "html.parser")
 print("""<?xml version="1.0" encoding="utf-8" ?><rss version="2.0" xml:base="https://www.whitehouse.gov/blog-daily-listings-rss" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom"><channel><title>White House - What's Happening</title><link>https://www.whitehouse.gov/blog-daily-listings-rss</link><description></description><language>en</language><atom:link href="https://inkdroid.org/rss/whitehouse.xml" rel="self" type="application/rss+xml" />""", end="") 
 
 for e in doc.select('.views-row'):
-    print(get_item(e), end="")
+    item = get_item(e)
+    if item:
+        print(item, end="")
 
 print("</channel></rss>", end="")
